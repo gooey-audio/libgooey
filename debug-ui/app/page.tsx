dbfda63a4dac5c31ffff35512646a3e62d54b20d
Brian Hurlow <brian@brianhurlow.com>
2025-12-29 18:01:52 -0800

    rm react based debug ui, due to limitations with wasm web audio. Also web audio api is better fit, so planning to maintain similar js lib

--- debug-ui/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import WasmTest from './wasm-test';
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-900 text-white">
-      <WasmTest />
-    </div>
-  );
-}
